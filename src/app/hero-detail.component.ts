14b5e9ca9093dde80a0ec1d815823dbddbb556d4
divvu <divyansh.dhamaniya@gmail.com>
2017-10-17 16:08:14 +0530

    added parameterized routing

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -7,16 +7,7 @@ import 'rxjs/add/operator/switchMap';
 
 @Component({
    selector: 'hero-detail',
-   template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name"/>
-      </div>
-    </div>
-   `
+   templateUrl: './hero-detail.component.html' 
 })
 
 export class HeroDetailComponent implements OnInit{
@@ -29,7 +20,7 @@ export class HeroDetailComponent implements OnInit{
   	this.route.paramMap
   	    .switchMap((params: ParamMap)  => 
   	this.heroService.getHero(+params.get('id')))
-  	    .subscribe((hero) => this.hero = hero);
+  	    .subscribe( hero => this.hero = hero);
   }
   goBack(): void{
   	this.location.back();
